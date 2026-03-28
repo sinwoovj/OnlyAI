5ce0b3758861b696af7c497b0e1dcbd068edd689
DESKTOP-DEO7RV9\rshh0 <a01043030339@gmail.com>
2026-03-28 23:59:31 +0900

    end

---
+++ Assets/Script/MovingObject.cs
@@ -0,0 +1,14 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public Transform startPos, endPos;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        float t = (Mathf.Cos(Time.time * speed) + 1f) * 0.5f;
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+    }
+}
